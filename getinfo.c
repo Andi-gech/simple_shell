21395a1558805499664f31f6159aceef48430fd9
Andi-gech <andi.fab23@gmail.com>
2022-12-23 11:16:07 -0800

    first

--- getinfo.c
+++ getinfo.c
@@ -30,18 +30,18 @@ if (!info->argv)
 
 info->argv = malloc(sizeof(char *) * 2);
 if (info->argv)
- {
-	      info->argv[0] = _strdup(info->arg);
-	      info->argv[1] = NULL;
-	    }
-	}
-      for (i = 0; info->argv && info->argv[i]; i++)
-	;
-      info->argc = i;
+{
+info->argv[0] = _strdup(info->arg);
+info->argv[1] = NULL;
+}
+}
+for (i = 0; info->argv && info->argv[i]; i++)
+;
+info->argc = i;
 
-      replace_alias(info);
-      replace_vars(info);
-    }
+replace_alias(info);
+replace_vars(info);
+}
 }
 
 /**
@@ -51,24 +51,24 @@ if (info->argv)
  */
 void free_info(info_t *info, int all)
 {
-  ffree(info->argv);
-  info->argv = NULL;
-  info->path = NULL;
-  if (all)
-    {
-      if (!info->cmd_buf)
-	free(info->arg);
-      if (info->env)
-	free_list(&(info->env));
-      if (info->history)
-	free_list(&(info->history));
-      if (info->alias)
-	free_list(&(info->alias));
-      ffree(info->environ);
-      info->environ = NULL;
-      bfree((void **)info->cmd_buf);
-      if (info->readfd > 2)
-	close(info->readfd);
-      _putchar(BUF_FLUSH);
-    }
+ffree(info->argv);
+info->argv = NULL;
+info->path = NULL;
+if (all)
+{
+if (!info->cmd_buf)
+free(info->arg);
+if (info->env)
+free_list(&(info->env));
+if (info->history)
+free_list(&(info->history));
+if (info->alias)
+free_list(&(info->alias));
+ffree(info->environ);
+info->environ = NULL;
+bfree((void **)info->cmd_buf);
+if (info->readfd > 2)
+close(info->readfd);
+_putchar(BUF_FLUSH);
+}
 }
